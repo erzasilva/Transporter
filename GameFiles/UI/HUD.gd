a336d64a0ee22401eec58074c7ef1b7a6351bcf9
erzasilva <erzasilva.93@gmail.com>
2022-07-01 19:39:29 +0530

    Basic Touch Input Complete

--- GameFiles/UI/HUD.gd
+++ GameFiles/UI/HUD.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 #onready vars
 onready var TouchJoyStick = $OuterRing
+onready var TouchIndicator = $InnerRing
 
 #signals
 signal Move_detected
@@ -9,17 +10,19 @@ signal Move_detected
 #helpers
 var move_vector = Vector2.ZERO
 var joystick_active = false
-func _input(event):
-	if event is InputEventScreenTouch or event is InputEventScreenDrag:
-		if TouchJoyStick.is_pressed():
-			var temp = event.position - (TouchJoyStick.position + Vector2(80, 80))
-			move_vector = temp.normalized()
-			joystick_active = true
-			emit_signal("Move_detected", move_vector)
+var TouchCenter
+
+func _ready():
+	TouchCenter = TouchJoyStick.position + Vector2(80, 80)
 	
+	
+func _input(event):
+	if event is InputEventScreenDrag or event is InputEventScreenTouch:
+		if TouchJoyStick.is_pressed():
+			move_vector = (event.position - TouchCenter).normalized()
+			emit_signal("Move_detected", move_vector)
+			
 	if event is InputEventScreenTouch:
-		if event.pressed == false:
-			joystick_active = false
+		if event.is_pressed() == false:
 			move_vector = Vector2.ZERO
 			emit_signal("Move_detected", move_vector)
-	
