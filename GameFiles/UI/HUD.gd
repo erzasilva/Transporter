4b6c185a5ed24f5a5733aa4c0e3bd27e61d6f75b
erzasilva <erzasilva.93@gmail.com>
2022-07-01 19:54:36 +0530

    Touch Joystick Complete
    Movement of Player and Touch Joystick complete

--- GameFiles/UI/HUD.gd
+++ GameFiles/UI/HUD.gd
@@ -11,9 +11,11 @@ signal Move_detected
 var move_vector = Vector2.ZERO
 var joystick_active = false
 var TouchCenter
+var IndicatorOffset = Vector2(36, 36)
 
 func _ready():
 	TouchCenter = TouchJoyStick.position + Vector2(80, 80)
+	TouchIndicator.rect_position = TouchCenter - IndicatorOffset
 	
 	
 func _input(event):
@@ -21,8 +23,11 @@ func _input(event):
 		if TouchJoyStick.is_pressed():
 			move_vector = (event.position - TouchCenter).normalized()
 			emit_signal("Move_detected", move_vector)
+			var TouchDistance = TouchCenter.distance_to(event.position)
+			TouchIndicator.rect_position = TouchCenter + (move_vector * clamp(TouchDistance, 0, 80)) - IndicatorOffset
 			
 	if event is InputEventScreenTouch:
 		if event.is_pressed() == false:
 			move_vector = Vector2.ZERO
 			emit_signal("Move_detected", move_vector)
+			TouchIndicator.rect_position = TouchCenter - IndicatorOffset
